2aff5f83e0e7b89e6445f24b21fad025065eb073
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-02 12:29:01 -0300

    Criando Contexto e Chamando via useContext

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 function DadosPessoais({ aoEnviar, validacoes }) {
