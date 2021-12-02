521612708d451a4d42d97e1396981bd53bb065fe
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-02 09:54:23 -0300

    Formulário

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import 'fontsource-roboto';
 
-import './App.css';
+import {Container, Typography } from "@material-ui/core"
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+      </Container>
+    );
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
 }
 
 export default App;
